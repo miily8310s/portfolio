b7507a58cdabc8c6d28f2afcd1de3e6b2e0b8cec
miily8310s <rillakerika@gmail.com>
2020-07-12 17:26:17 +0900

    add componets

---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="site">
+          <a href="/">miruo's Portfolio</a>
+        </div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="#">TOP</a>
+            </li>
+            <li>
+              <a href="#about">ABOUT</a>
+            </li>
+            <li>
+              <a href="#skill">SKILLS</a>
+            </li>
+            <li>
+              <a href="#project">PROJECTS</a>
+            </li>
+            <li>
+              <a href="#">LINKS</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
